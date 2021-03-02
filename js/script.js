cdd1c367d6b5a849e6829d9bf81257e647ae4672
Malena de Juan <malena.dj4@gmail.com>
2021-03-02 12:21:16 +0100

    palabras clonadas y copiadas al recuadro resultado en hacer click (con 1a mayus)

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const palabras = document.querySelectorAll(".lista-palabras li");
+const nuevasPalabras = [];
+
+for (const palabra of palabras) {
+  const nuevaPalabra = palabra.cloneNode(true);
+  palabra.addEventListener("click", () => {
+    nuevasPalabras.push(nuevaPalabra.textContent);
+    if (nuevaPalabra.textContent === nuevasPalabras[0]) {
+      nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
+    }
+    document.querySelector(".resultado").append(nuevaPalabra);
+  });
+}
