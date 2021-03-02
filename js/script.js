5b153bbcab086347a4563baffa70c06fafa6f200
Malena de Juan <malena.dj4@gmail.com>
2021-03-02 13:31:58 +0100

    correccion clonado de palabras dentro de evento click

--- js/script.js
+++ js/script.js
@@ -2,22 +2,24 @@ const palabras = document.querySelectorAll(".lista-palabras li");
 const nuevasPalabras = [];
 
 for (const palabra of palabras) {
-  const nuevaPalabra = palabra.cloneNode(true);
+  let nuevaPalabra;
   palabra.addEventListener("click", () => {
-    /*   if (document.querySelector(".resultado").children.length === 0) {
-        nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
-      } */
+    /*     if (document.querySelector(".resultado").children.length === 0) {
+      nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
+    } */
+    nuevaPalabra = palabra.cloneNode(true);
     nuevasPalabras.push(nuevaPalabra.textContent);
     if (nuevaPalabra.textContent === nuevasPalabras[0]) {
       nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
     }
     document.querySelector(".resultado").append(nuevaPalabra);
+    console.log(nuevasPalabras);
   });
-  nuevaPalabra.addEventListener("click", () => {
-    const posicionPalabra = nuevasPalabras.indexOf(nuevaPalabra.textContent);
-    nuevasPalabras.splice(posicionPalabra, 1);
-    nuevaPalabra.remove();
-  });
+  /*   nuevaPalabra.addEventListener("click", () => {
+      const posicionPalabra = nuevasPalabras.indexOf(nuevaPalabra.textContent);
+      nuevasPalabras.splice(posicionPalabra, 1);
+      nuevaPalabra.remove();
+    }); */
 }
 
 const botonSubmit = document.querySelector(".boton-submit");
