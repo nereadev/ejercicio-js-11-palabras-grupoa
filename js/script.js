6c7d56ef6bb19e7e9f79731d8bac09de0bc46205
Malena de Juan <malena.dj4@gmail.com>
2021-03-02 12:59:22 +0100

    se borran de pantalla las palabras de resultado en click

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ const nuevasPalabras = [];
 for (const palabra of palabras) {
   const nuevaPalabra = palabra.cloneNode(true);
   palabra.addEventListener("click", () => {
-    if (document.querySelector(".resultado").children.length === 0) {
-      nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
-    }
-    /* nuevasPalabras.push(nuevaPalabra.textContent);
+    /*   if (document.querySelector(".resultado").children.length === 0) {
+        nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
+      } */
+    nuevasPalabras.push(nuevaPalabra.textContent);
     if (nuevaPalabra.textContent === nuevasPalabras[0]) {
       nuevaPalabra.textContent = nuevaPalabra.textContent.charAt(0).toUpperCase() + nuevaPalabra.textContent.slice(1);
-    } */
+    }
     document.querySelector(".resultado").append(nuevaPalabra);
+  });
+  nuevaPalabra.addEventListener("click", () => {
+    const posicionPalabra = nuevasPalabras.indexOf(nuevaPalabra.textContent);
+    nuevasPalabras.splice(posicionPalabra, 1);
+    nuevaPalabra.remove();
   });
 }
 
